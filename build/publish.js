4292d80ea9b08570a19bab330d6662e723c991c6
caperaven@icloud.com <caperaven@icloud.com>
2016-11-09 22:38:40 +0200

    ts to js

--- build/publish.js
+++ build/publish.js
@@ -1,13 +1,21 @@
 const gulp = require("gulp");
 const Publish = require("build-utilities/publish");
 
-gulp.task('publish', ['build:all'], function() {
+gulp.task('publish', ['build:all', 'build:dist'], function() {
     const publish = new Publish();
 
     publish.publish([
         {
-            src: ["app/src/lib/**/*.*"],
-            dest: "publish"
+            src: ["distribute/amd/lib/**/*.*"],
+            dest: "publish/amd"
+        },
+        {
+            src: ["distribute/commonjs/lib/**/*.*"],
+            dest: "publish/commonjs"
+        },
+        {
+            src: ["distribute/systemjs/lib/**/*.*"],
+            dest: "publish/systemjs"
         }
     ]);
 
